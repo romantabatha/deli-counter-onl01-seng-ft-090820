c77c5bd185ce22b7b98c18cbef2340043cbe5526
Tabatha Roman <romantabatha17@gmail.com>
2020-09-12 03:10:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+
+def line(katz_deli)
+  if katz_deli == 0 
+   puts "The line is currently empty."
+  end
+end
+
+# def take_a_number(katz_deli,"name")
+  
+# end
+
+# def now_serving
+
+# end
