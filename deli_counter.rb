ce8ebdb0d26b6ba5df3bfeebaeec63573ca05a2d
Tabatha Roman <romantabatha17@gmail.com>
2020-09-12 03:10:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,14 @@
 
-def line(katz_deli)
-  if katz_deli == 0 
-   puts "The line is currently empty."
-  end
-end
+# def line(katz_deli)
+#   if katz_deli == 0 
+#   puts "The line is currently empty."
+#   end
+# end
 
-# def take_a_number(katz_deli,"name")
+# # def take_a_number(katz_deli,"name")
   
-# end
+# # end
 
-# def now_serving
+# # def now_serving
 
-# end
+# # end
