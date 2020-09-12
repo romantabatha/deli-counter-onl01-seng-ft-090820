174722846d73caa3eeb47bd3748170ac0045e6ea
Tabatha Roman <romantabatha17@gmail.com>
2020-09-12 03:13:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 
 def line(katz_deli)
-  phrase = "The line is currently: "
+  phrase = "The line is currently:"
   if katz_deli.length > 0 
     katz_deli.each_with_index do |name, index|
       phrase += "#{index + 1}. #{name} "
